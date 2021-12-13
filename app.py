63361bd364e50ec7bb0466df78e261f1eee8060f
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2021-12-13 10:59:41 +0100

    modification de acceuil en un layout

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ def form():
 @app.route('/post',methods=['post'])
 def post():
      form_data=request.form.to_dict()
-     print(form_data)
      subs = sqlite3.connect('sub.db')
      cursor = subs.cursor()
      cursor.execute("""
      INSERT INTO table1(Nom,Mots_clés) values(?,?)""",(str(form_data['name']),str(form_data['domaine'])))
      subs.commit()
-     return render_template('sub.html')
-    
+     return render_template('accueil.html')
+
+if __name__=='__main__':
+     app.run(debug=1)

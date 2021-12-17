557acdf16ab9a72fddf8620c84294e65a3b0e315
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2021-12-17 14:43:59 +0100

    recherche de sub + modif bd

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for,redirect,flash
+import datetime
 
 app = Flask(__name__)
 
@@ -17,9 +18,47 @@ def post():
      subs = sqlite3.connect('sub.db')
      cursor = subs.cursor()
      cursor.execute("""
-     INSERT INTO table1(Nom,Mots_clés) values(?,?)""",(str(form_data['name']),str(form_data['domaine'])))
+     INSERT INTO table1(Nom,Posté_par,Mots_clés,description,création) values(?,?,?,?,?)""",(str(form_data['name']),'admin',str(form_data['domaine']),str(form_data['description']),datetime.date.today()))
      subs.commit()
+     subs.close()
+     return redirect('/')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def recherche():
+    search = request.form.to_dict()
+    if request.method == 'POST':
+        return search_results(search)
+    return render_template('resultat.html', form=search)
+
+
+@app.route('/results')
+def search_results(search):
+     resultat = []
+     search_str = search['Search']
+     subs = sqlite3.connect('sub.db')
+     cursor = subs.cursor()
+     cursor.execute("""SELECT * FROM table1""")
+     contenu=cursor.fetchall()
+     resultat=[]
+     for row in contenu:
+          if search_str in row[0] or search_str in row[3]:
+               date=row[5].split('-')
+               Y=int(date[0])
+               M=int(date[1])
+               D=int(date[2])
+               resultat.append(row[:-1]+(str((datetime.date.today()-datetime.date(Y,M,D)).days)+' days ago',))
+               
+     subs.close()
+     if not resultat:
+         return render_template('resultat.html',resultat='')
+     else:
+         return render_template('resultat.html', resultat=resultat)
+
+@app.route('/sub/<id>')
+def sub(id):
      return render_template('accueil.html')
+
 
 if __name__=='__main__':
      app.run(debug=1)

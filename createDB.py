321715f2f5cb3f6090e2e6a883db2ca147081c94
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2021-12-20 18:49:32 +0100

    MAJ des BD

--- createDB.py
+++ createDB.py
@@ -1,11 +1,10 @@
 import sqlite3
 
-def createDB(id):
-    nom_db = f"post_{id}"
-    db = sqlite3.connect('post.db')
+def createDB():
+    db = sqlite3.connect('database.db')
     cursor = db.cursor()
-    args = (nom_db,)
-    cursor.execute("CREATE TABLE %s(id_post INTEGER PRIMARY KEY AUTOINCREMENT, id_sub INTEGER, titre TEXT, description TEXT, date_creation DATE, nbr_visites INTEGER, FOREIGN KEY(id_sub) REFERENCES table1(Numéro_projet));" % nom_db) 
+    cursor.execute("CREATE TABLE posts (id_post INTEGER PRIMARY KEY AUTOINCREMENT, id_sub INTEGER, titre TEXT, description TEXT, date_creation DATE, nbr_visites INTEGER, FOREIGN KEY(id_sub) REFERENCES table1(Numéro_projet));")
     db.commit()
     db.close()
 
+createDB()

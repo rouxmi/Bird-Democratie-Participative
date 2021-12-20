bfd5b6373f38709e9a22487b22bf0a5da86256e6
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2021-12-20 19:47:48 +0100

    MAJ

--- createDB.py
+++ createDB.py
@@ -3,8 +3,6 @@ import sqlite3
 def createDB():
     db = sqlite3.connect('database.db')
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE posts (id_post INTEGER PRIMARY KEY AUTOINCREMENT, id_sub INTEGER, titre TEXT, description TEXT, date_creation DATE, ratio INTEGER, FOREIGN KEY(id_sub) REFERENCES table1(Numéro_projet));")
+    cursor.execute("CREATE TABLE posts (id_post INTEGER PRIMARY KEY AUTOINCREMENT, id_sub INTEGER, titre TEXT, description TEXT, date_creation DATE, ratio INTEGER, FOREIGN KEY(id_sub) REFERENCES subs(Numéro_projet));")
     db.commit()
     db.close()
-
-createDB()

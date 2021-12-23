c573074e4103f3c7504555cb20878c9d5d13874a
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2021-12-23 13:49:44 +0100

    Ajout de la fonction s'abboner

--- createDB.py
+++ createDB.py
@@ -1,9 +1,17 @@
 import sqlite3
 
-def createDB():
+def createDBposts():
     db = sqlite3.connect('database.db')
     cursor = db.cursor()
     cursor.execute("CREATE TABLE posts (id_post INTEGER PRIMARY KEY AUTOINCREMENT, id_sub INTEGER, titre TEXT, description TEXT, posté_par INTEGER, date_creation DATE, ratio INTEGER, FOREIGN KEY(id_sub) REFERENCES subs(Numéro_projet));")
     db.commit()
     db.close()
-createDB()
+
+
+def createDBabonnements():
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE abonnements (sub INTEGER NOT NULL,utilisateur INTEGER NOT NULL, PRIMARY KEY(sub,utilisateur),FOREIGN KEY(sub) REFERENCES subs(numéro_projet),FOREIGN KEY(utilisateur) REFERENCES utilisateurs(id_user));")
+    db.commit()
+    db.close()
+

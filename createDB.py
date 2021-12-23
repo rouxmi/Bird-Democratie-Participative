fb59530008fea609a931b054a072f4b5c25c4063
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2021-12-23 11:11:37 +0100

    Ajout de la colonne posté_par dans la table posts

--- createDB.py
+++ createDB.py
@@ -3,6 +3,7 @@ import sqlite3
 def createDB():
     db = sqlite3.connect('database.db')
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE posts (id_post INTEGER PRIMARY KEY AUTOINCREMENT, id_sub INTEGER, titre TEXT, description TEXT, date_creation DATE, ratio INTEGER, FOREIGN KEY(id_sub) REFERENCES subs(Numéro_projet));")
+    cursor.execute("CREATE TABLE posts (id_post INTEGER PRIMARY KEY AUTOINCREMENT, id_sub INTEGER, titre TEXT, description TEXT, posté_par INTEGER, date_creation DATE, ratio INTEGER, FOREIGN KEY(id_sub) REFERENCES subs(Numéro_projet));")
     db.commit()
     db.close()
+createDB()

d2016b8ed38c1dd2915dfd0ae1ff067ebbd74291
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2022-01-05 14:25:48 +0100

    correction test

--- pytest/test_upvote.py
+++ pytest/test_upvote.py
@@ -2,14 +2,14 @@ import pytest
 import app
 
 def connect(ses):
-    ses.post('/connect',data={'username':'LEFEBVRE.Emma@gmail.com','password':'qS8rb5l9guE'},follow_redirects=True)
+    ses.post('/connect',data={'username':'ROUX.Ambre@gmail.com','password':'iaCDoj06BXUp'},follow_redirects=True)
 
 @pytest.fixture
 def base():
     with app.app.test_client() as test:
         yield test
 
-@pytest.mark.parametrize('id',[(7),(23),(54),(12),(17),(35),(43),(34),(42),('a')])
+@pytest.mark.parametrize('id',[(7),(23),(54),(12),(17),(35),(43),(34),(42),('a'),(0)])
 #cross check en regardant dans la BD
 def test_upvote(base,id):
     connect(base)

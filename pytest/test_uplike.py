a2cc9df4db8eaf51718c6cda7ece578192560e7c
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2022-01-05 14:44:11 +0100

    test V8

---
+++ pytest/test_uplike.py
@@ -0,0 +1,18 @@
+import pytest
+import app
+
+def connect(ses):
+    ses.post('/connect',data={'username':'ROUX.Ambre@gmail.com','password':'iaCDoj06BXUp'},follow_redirects=True)
+
+@pytest.fixture
+def base():
+    with app.app.test_client() as test:
+        yield test
+
+@pytest.mark.parametrize('id',[(7),(23),(54),(12),(17),(35),(43),(34),(42),('a'),(0)])
+#cross check en regardant dans la BD
+def test_uplike(base,id):
+    connect(base)
+    response = base.get('/uplike/'+str(id))
+    assert response.status_code == 302 or response.status_code == 200
+

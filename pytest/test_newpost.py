c079f30441fb606e7c10a8b6c481da0a5da02547
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2022-01-05 13:48:51 +0100

    Ajout de la fonction test_newpost

---
+++ pytest/test_newpost.py
@@ -0,0 +1,18 @@
+import pytest
+from app import desabonnement
+import app
+
+def connect(ses):
+    ses.post('/connect',data={'username':'ROUX.Ambre@gmail.com','password':'iaCDoj06BXUp'},follow_redirects=True)
+
+@pytest.fixture
+def base():
+    with app.app.test_client() as test_client:
+        yield test_client
+
+@pytest.mark.parametrize('id',[24,35,6,0,None])
+
+def test_(base,id):
+    connect(base)
+    response = base.get('/sub/{id}/creationpost')
+    assert response.status_code == 302

47dba0e469f9624134631cd2701b893ab0f6da10
Eleve 1A Telecom Nancy <louis.cleriot@telecomnancy.eu>
2022-01-05 13:06:58 +0100

    test fonction recherche

---
+++ pytest/test_search_results.py
@@ -0,0 +1,38 @@
+import sqlite3
+from flask.templating import render_template
+import pytest
+import datetime
+import app
+
+def connect(ses):
+    ses.post('/connect',data={'username':'MICHEL.Louise@gmail.com','password':'qS8rb5l9guEB'},follow_redirects=True)
+
+@pytest.fixture
+def base():
+    with app.app.test_client() as test:
+        yield test
+
+subs = sqlite3.connect('database.db')
+cursor = subs.cursor()
+cursor.execute("""SELECT nom,mots_clés,description,création FROM subs where numéro_projet = 28""")
+contenu=cursor.fetchall()
+print(contenu)
+date=contenu[0][3].split('-')
+resultat=[]
+Y=int(date[0])
+M=int(date[1])
+D=int(date[2])
+
+resultat.append(contenu[0][:-1]+(str((datetime.date.today()-datetime.date(Y,M,D)).days)+' days ago',))
+
+@pytest.mark.parametrize('search,expect',[('orn',''),('matin',resultat)])
+
+def test_(base,search,expect):
+    connect(base)
+    response = base.post('/search',data={'Search':search})
+    assert response.status_code == 200
+    if type(expect)==type([]):
+        for i in range(len(expect[0])):
+            assert str(expect[0][i]).encode('UTF-8') in response.data
+    else:
+        assert expect.encode('UTF-8') in response.data
